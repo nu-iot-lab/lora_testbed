5d0e7a31d403defc69aa8e9f55547a6d8704fadc
Dimitrios Zorbas <dimitrios.zormpas@tyndall.ie>
2023-01-31 19:36:34 +0600

    first commit

---
+++ end-device/main.py
@@ -0,0 +1,206 @@
+from machine import SoftI2C, Pin, SPI, reset, idle
+from lora import LoRa
+import ssd1306
+import time
+from time import sleep
+import socket
+import struct
+import network
+import ubinascii
+import uos
+import _thread
+import uerrno
+import sys
+import random
+import uhashlib
+
+# led = Pin(25,Pin.OUT) # heltec V2
+led = Pin(2,Pin.OUT) # TTGO
+rst = Pin(16, Pin.OUT)
+rst.value(1)
+scl = Pin(15, Pin.OUT, Pin.PULL_UP)
+sda = Pin(4, Pin.OUT, Pin.PULL_UP)
+i2c = SoftI2C(scl=scl, sda=sda, freq=450000)
+oled = ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3c)
+oled.poweron()
+oled.fill(0)
+oled.text("LoRa testbed", 0, 0)
+oled.show()
+
+# SPI pins
+SCK  = 5
+MOSI = 27
+MISO = 19
+CS   = 18
+RX   = 26
+
+# Setup SPI
+spi = SPI(
+    1,
+    baudrate=1000000,
+    sck=Pin(SCK, Pin.OUT, Pin.PULL_DOWN),
+    mosi=Pin(MOSI, Pin.OUT, Pin.PULL_UP),
+    miso=Pin(MISO, Pin.IN, Pin.PULL_UP),
+)
+spi.init()
+
+# Setup LoRa
+lora = LoRa(
+    spi,
+    cs=Pin(CS, Pin.OUT),
+    rx=Pin(RX, Pin.IN),
+)
+
+# some settings
+freqs = [868.1, 868.3, 868.5, 867.1, 867.3, 867.5, 867.7, 867.9]
+rx2freq = 869.525
+lora.set_spreading_factor(7)
+lora.set_frequency(freqs[0])
+wlan = network.WLAN(network.STA_IF)
+lora.standby()
+
+# some global variables (values will be overriden later)
+mac = "FFFFFFFFFFFF"
+dev_id = 1000000
+last_seq = -1
+_start_experiment = 1
+_pkts = 10
+_pkt_size = 16
+_period = 60
+_sf = 7
+_confirmed = 1
+
+### --- FUNCTIONS --- ###
+def convert_mac(mac):
+    # first 24 bits = OUI
+    addr = mac[-6:]
+    print(addr)
+    return int(addr, 16)
+
+def oled_lines(line1, line2, line3, line4):
+    oled.fill(0)
+    oled.text(line1, 0, 0)
+    oled.text(line2, 0, 10)
+    oled.text(line3, 0, 20)
+    oled.text(line4, 0, 30)
+    oled.show()
+
+def wifi_connect():
+    global wlan, mac, dev_id
+    wlan.active(True)
+    mac = ubinascii.hexlify(wlan.config('mac')).decode().upper()
+    dev_id = convert_mac(mac)
+    mac = ':'.join(mac[i:i+2] for i in range(0,12,2))
+    print("MAC address:", mac)
+    oled_lines("LoRa testbed", mac, " ", " ")
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('rasp', 'lalalala')
+        while not wlan.isconnected():
+            pass
+
+def wait_commands():
+    global _start_experiment, _pkts, _pkt_size, _period, _confirmed
+    wifi_connect()
+    host = wlan.ifconfig()[0]
+    port = 8000
+    wlan_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    wlan_s.bind((host, port))
+    wlan_s.listen(5)
+    print("Ready...")
+    while (True):
+        conn, addr = wlan_s.accept()
+        data = conn.recv(512)
+        if (len(data) > 10):
+            try:
+                (init, _pkts, _pkt_size, _period, _confirmed) = struct.unpack('BiiiB', data)
+                if (init > 0):
+                    print("---------------------------------")
+                    print("New experiment with", pkts, "packets")
+                    _start_experiment = init
+            except:
+                print("wrong packet format!")
+
+def generate_msg():
+    msg = random.getrandbits(32) # just a random 4-byte int
+    msg = hex(msg)[2:]
+    while (len(msg) < _pkt_size):
+        msg = msg + msg
+    while (len(msg) > _pkt_size): # just correct the size
+        msg = msg[:-1]
+    return msg
+
+def random_sleep(max_sleep):
+    t = random.getrandbits(32)
+    time.sleep(1+t%max_sleep)
+
+def rx_handler(recv_pkg):
+    global ack, last_seq
+    if (len(recv_pkg) > 2):
+        recv_pkg_len = recv_pkg[1]
+        try:
+            (gw_id, dev_id, seq) = struct.unpack("iii", recv_pkg)
+            print('Received response from', hex(gw_id))
+            if (int(dev_id, 16) == _dev_id) and (seq == last_seq):
+                ack = 1
+        except:
+            print("wrong packet format!")
+
+# _thread.start_new_thread(wait_commands, ())
+
+while(True):
+    # time.sleep(10)
+    if (_start_experiment):
+        print("Random sleep time")
+        # random_sleep(_period)
+        lora.standby()
+        pkts = 1
+        _start_experiment = 0
+        while(pkts <= _pkts and _start_experiment == 0):
+            print("-------",pkts,"-------")
+            data = generate_msg()
+            cks = uhashlib.sha256(data)
+            cks = cks.digest()
+            cks = ubinascii.hexlify(cks)[8:]
+            last_seq = pkts
+            data = struct.pack('I%dsii' % len(data), dev_id, data, int(cks, 16), pkts)
+            lora.send(data)
+            last_trans = time.ticks_ms()
+            ack = 0
+            if (_confirmed):
+                time.sleep_ms(980)
+                lora.on_recv(rx_handler)
+                lora.recv()
+                recv_time = time.ticks_ms()
+                led.value(1)
+                while(time.ticks_diff(time.ticks_ms(), recv_time) < 200):
+                    if (ack):
+                        break
+                if (ack):
+                    delivered += 1
+                else:
+                    lora.sleep()
+                    led.value(0)
+                    print("No ack was received in RX1")
+                    time.sleep_ms( time.ticks_diff(last_trans+1980, time.ticks_ms()) )
+                    lora.set_spreading_factor(12)
+                    lora.set_frequency(rx2freq)
+                    lora.recv()
+                    led.value(1)
+                    while(time.ticks_diff(time.ticks_ms(), recv_time) < 200):
+                        if (ack):
+                            break
+                    if (ack):
+                        delivered += 1
+                    else:
+                        pkts -= 1
+                        print("No ack was received in RX2")
+            lora.set_spreading_factor(_sf)
+            lora.set_frequency(freqs[0])
+            led.value(0)
+            lora.sleep()
+            pkts += 1
+            # watch duty cycle violations here
+            time.sleep_ms(_period*1000)
+            random_sleep(2) # sleep for some random time as well
+        # TODO: send statistics
---
+++ gateway/main.py
@@ -0,0 +1,167 @@
+from machine import SoftI2C, Pin, SPI, reset, idle
+from lora import LoRa
+import ssd1306
+import time
+from time import sleep
+import socket
+import struct
+import network
+import ubinascii
+import uos
+import _thread
+import uerrno
+import sys
+import random
+import uhashlib
+
+# led = Pin(25,Pin.OUT) # heltec V2
+led = Pin(2,Pin.OUT) # TTGO
+rst = Pin(16, Pin.OUT)
+rst.value(1)
+scl = Pin(15, Pin.OUT, Pin.PULL_UP)
+sda = Pin(4, Pin.OUT, Pin.PULL_UP)
+i2c = SoftI2C(scl=scl, sda=sda, freq=450000)
+oled = ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3c)
+oled.poweron()
+oled.fill(0)
+oled.text("LoRa testbed", 0, 0)
+oled.show()
+
+# SPI pins
+SCK  = 5
+MOSI = 27
+MISO = 19
+CS   = 18
+RX   = 26
+
+# Setup SPI
+spi = SPI(
+    1,
+    baudrate=1000000,
+    sck=Pin(SCK, Pin.OUT, Pin.PULL_DOWN),
+    mosi=Pin(MOSI, Pin.OUT, Pin.PULL_UP),
+    miso=Pin(MISO, Pin.IN, Pin.PULL_UP),
+)
+spi.init()
+
+# Setup LoRa
+lora = LoRa(
+    spi,
+    cs=Pin(CS, Pin.OUT),
+    rx=Pin(RX, Pin.IN),
+)
+
+# some settings
+freqs = [868.1, 868.3, 868.5, 867.1, 867.3, 867.5, 867.7, 867.9]
+rx2freq = 869.525
+lora.set_spreading_factor(7)
+lora.set_frequency(freqs[0])
+wlan = network.WLAN(network.STA_IF)
+
+# some global variables (values will be overriden later)
+mac = "FFFFFFFFFFFF"
+gw_id = 1000000
+schedule = []
+next_avail_slot = 0.0
+next_duty_cycle = {}
+next_duty_cycle[1] = 0.0
+next_duty_cycle[2] = 1.0
+_sf = 7
+_rx2sf = 9
+
+### --- FUNCTIONS --- ###
+def convert_mac(mac):
+    # first 24 bits = OUI
+    addr = mac[-6:]
+    print(addr)
+    return int(addr, 16)
+
+def oled_lines(line1, line2, line3, line4):
+    oled.fill(0)
+    oled.text(line1, 0, 0)
+    oled.text(line2, 0, 10)
+    oled.text(line3, 0, 20)
+    oled.text(line4, 0, 30)
+    oled.show()
+
+def wifi_connect():
+    global wlan, mac, gw_id
+    wlan.active(True)
+    mac = ubinascii.hexlify(wlan.config('mac')).decode().upper()
+    gw_id = convert_mac(mac)
+    mac = ':'.join(mac[i:i+2] for i in range(0,12,2))
+    print("MAC address:", mac)
+    oled_lines("LoRa testbed", mac, " ", " ")
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('rasp', 'lalalala')
+        while not wlan.isconnected():
+            pass
+
+# this is borrowed from LoRaSim (https://www.lancaster.ac.uk/scc/sites/lora/lorasim.html)
+def airtime(sf,cr,pl,bw):
+    H = 0        # implicit header disabled (H=0) or not (H=1)
+    DE = 0       # low data rate optimization enabled (=1) or not (=0)
+    Npream = 8
+    if bw == 125 and sf in [11, 12]:
+        # low data rate optimization mandated for BW125 with SF11 and SF12
+        DE = 1
+    if sf == 6:
+        # can only have implicit header with SF6
+        H = 1
+    Tsym = (2.0**sf)/bw
+    Tpream = (Npream + 4.25)*Tsym
+    payloadSymbNB = 8 + max(math.ceil((8.0*pl-4.0*sf+28+16-20*H)/(4.0*(sf-2*DE)))*(cr+4),0)
+    Tpayload = payloadSymbNB * Tsym
+    return Tpream + Tpayload
+
+
+def rx_handler(recv_pkg):
+    global schedule
+    if (len(recv_pkg) > 2):
+        recv_pkg_len = recv_pkg[1]
+        try:
+            (dev_id, data, cks, seq) = struct.unpack("I%dsii", recv_pkg)
+            recv_time = time.ticks_ms()
+            check = uhashlib.sha256(data)
+            check = check.digest()
+            ckeck = ubinascii.hexlify(check)[8:]
+            if (check == cks):
+                # schedule an ack if the ack does not collide with other transmissions and there are available resources
+                if (recv_time+1000 > next_avail_slot) and (next_duty_cycle[1] <= recv_time+1000):
+                    airt = airtime(_sf,1,12,125)
+                    schedule.append([recv_time+980, dev_id, seq, 1])
+                    next_avail_slot = recv_time + 1000 + airt
+                elif (recv_time+2000 > next_avail_slot) and (next_duty_cycle[2] <= recv_time+2000):
+                    airt = airtime(_rx2sf,1,12,125)
+                    schedule.append([recv_time+1980, dev_id, seq, 2])
+                else:
+                    print("Gateway unavailable!")
+            else:
+                print("Checksum not valid!")
+        except:
+            print("wrong packet format!")
+
+def scheduler():
+    global schedule, next_duty_cycle
+    while(1):
+        if len(schedule) > 0:
+            (tm, dev_id, seq, win) = schedule[0]
+            ack_pkt = struct.pack('iii', gw_id, dev_id, seq)
+            while(time.ticks_diff(time.ticks_ms(), tm) > 0):
+                machine.idle()
+            if (win == 2):
+                lora.set_spreading_factor(_rx2sf)
+                lora.set_frequency(rx2freq)
+            lora.send(ack_pkt)
+            if (win == 1):
+                next_duty_cycle[2] = time.ticks_ms()+99*airtime(_sf,1,12,125)
+            elif (win == 2):
+                next_duty_cycle[2] = time.ticks_ms()+9*airtime(_rx2sf,1,12,125)
+            schedule.remove(schedule[0])
+            lora.recv()
+
+lora.on_recv(rx_handler)
+lora.recv()
+
+_thread.start_new_thread(scheduler, ())
