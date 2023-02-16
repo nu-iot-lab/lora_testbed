734060bd0ba9ffd61f6723d18b0fbe25795b865f
Dimitrios Zorbas <dimitrios.zormpas@tyndall.ie>
2023-02-16 19:31:01 +0600

    webrepl working version

--- initiator/init_exp_f.py
+++ initiator/init_exp_f.py
@@ -8,6 +8,10 @@ import subprocess
 import shlex
 import time
 
+if len(sys.argv) < 3:
+	print("python3 init_exp_f.py -m <U/C>")
+	sys.exit()
+
 PORT = 8000
 BUFFER_SIZE = 512
 assets = []
@@ -19,11 +23,6 @@ with open("assets.txt") as file:
 	next(file)
 	assets = [l.rstrip() for l in file]
 
-
-# if the argument is 'U'
-# 	-> send webrepl command to each device (ED / GW)
-# 	-> wait 30" and then continue
-# else continue
 
 for i in range(len(sys.argv)):
 	if sys.argv[i] == '-m':
@@ -60,8 +59,6 @@ if mode == 'U':
 			time.sleep(3)
 			reboot_process.kill()
 			print(" ######### REBOOT FINISHED  ###########   ")
-            
-            
 		elif (items[0] == 'ED'):
 			# launch send script with args for the GW
 
@@ -84,47 +81,46 @@ if mode == 'U':
 			time.sleep(3)
 			reboot_process.kill()
 			print(" ######### REBOOT FINISHED  ###########   ")
-            
 		else:
 			continue
-
-for asset in assets:
-	MESSAGE = bytes(0)
-	print(asset)
-	items = asset.split(" ")
-	IP = items[1]
-	if (items[0] == 'GW'):
-		MESSAGE = struct.pack('HBB', init, int(items[2]), int(items[3]))
-	elif (items[0] == 'ED'):
-		eds += 1
-		MESSAGE = struct.pack('HiiiBBB', init, int(items[2]), int(items[3]), int(items[4]), int(items[5]), int(items[6]), int(items[7]))
-	else:
-		continue
-	print("Sending to", items[0])
-	try:
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((IP, PORT))
-		s.send( MESSAGE )
-		s.close()
-	except:
-		print("Socket error!")
-
-
-print("\nWaiting for statistics\n")
-f = open("stats.txt", "w")
-a = 0
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('192.168.1.230', PORT))
-s.listen(50)
-while (a < eds):
-	(conn, addr) = s.accept()
-	data = conn.recv(512)
-	if (len(data) > 10):
+elif mode == 'C':
+	for asset in assets:
+		MESSAGE = bytes(0)
+		print(asset)
+		items = asset.split(" ")
+		IP = items[1]
+		if (items[0] == 'GW'):
+			MESSAGE = struct.pack('HBB', init, int(items[2]), int(items[3]))
+		elif (items[0] == 'ED'):
+			eds += 1
+			MESSAGE = struct.pack('HiiiBBB', init, int(items[2]), int(items[3]), int(items[4]), int(items[5]), int(items[6]), int(items[7]))
+		else:
+			continue
+		print("Sending to", items[0])
 		try:
-			(id, deliv, fail, rssi) = struct.unpack('IIIf', data)
-			print(hex(id), deliv, fail, rssi)
-			f.write( "%s: %s %s %s\n" % ( hex(id), str(deliv), str(fail), str(rssi) ) )
-			a += 1
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((IP, PORT))
+			s.send( MESSAGE )
+			s.close()
 		except:
-			print("wrong stat packet format!")
-f.close()
+			print("Socket error!")
+
+
+	print("\nWaiting for statistics\n")
+	f = open("stats.txt", "w")
+	a = 0
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind(('192.168.1.230', PORT))
+	s.listen(50)
+	while (a < eds):
+		(conn, addr) = s.accept()
+		data = conn.recv(512)
+		if (len(data) > 10):
+			try:
+				(id, deliv, fail, rssi) = struct.unpack('IIIf', data)
+				print(hex(id), deliv, fail, rssi)
+				f.write( "%s: %s %s %s\n" % ( hex(id), str(deliv), str(fail), str(rssi) ) )
+				a += 1
+			except:
+				print("wrong stat packet format!")
+	f.close()
